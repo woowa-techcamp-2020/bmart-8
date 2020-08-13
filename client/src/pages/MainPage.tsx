67f99829adc636ec217930f7d0032122b547c31c
nohgijin <ynda11111@naver.com>
2020-08-13 21:10:58 +0900

    chore: 카테고리 아이콘 이미지 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Logo from '../components/Logo';
 const MainPageBlock = styled.div``;
 
 const MainPage: React.FC = () => {
-  return <MainPageBlock>MainPage</MainPageBlock>;
+  return (
+    <MainPageBlock>
+      <Logo></Logo>
+    </MainPageBlock>
+  );
 };
 
 export default MainPage;
