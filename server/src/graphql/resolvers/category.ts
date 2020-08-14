007851b090c541c6b9ac11d1e81004633fb713be
hanSong2 <rlagksd@naver.com>
2020-08-14 15:49:51 +0900

    refactor: prisma 모델명 변경 동기화

--- server/src/graphql/resolvers/category.ts
+++ server/src/graphql/resolvers/category.ts
@@ -4,33 +4,33 @@ const prisma = new PrismaClient();
 export default {
   Query: {
     firstCategories: () =>
-      prisma.category_First.findMany({
+      prisma.category_first.findMany({
         include: { children: true },
       }),
     firstCategory: (parent: any, { id }: { id: string }) =>
-      prisma.category_First.findOne({
+      prisma.category_first.findOne({
         where: { id: parseInt(id) },
       }),
     secondCategories: () =>
-      prisma.category_Second.findMany({
+      prisma.category_second.findMany({
         include: { parent: true, children: true },
       }),
     secondCategory: (parent: any, { id }: { id: string }) =>
-      prisma.category_Second.findOne({
+      prisma.category_second.findOne({
         where: { id: parseInt(id) },
         include: { parent: true, children: true },
       }),
     thirdCategories: () =>
-      prisma.category_Third.findMany({
+      prisma.category_third.findMany({
         include: { parent: { include: { parent: true } } },
       }),
     thirdCategory: (parent: any, { id }: { id: string }) =>
-      prisma.category_Third.findOne({
+      prisma.category_third.findOne({
         where: { id: parseInt(id) },
         include: { parent: true },
       }),
     categories: async () => {
-      const categories = await prisma.category_Third.findMany({
+      const categories = await prisma.category_third.findMany({
         include: { parent: { include: { parent: true } } },
       });
       return categories.map((category) => {
@@ -44,14 +44,14 @@ export default {
   },
   Mutation: {
     CreateFirstCategory: (parent: any, args: any) => {
-      return prisma.category_First.create({
+      return prisma.category_first.create({
         data: {
           name: args.name,
         },
       });
     },
     CreateSecondCategory: (parent: any, args: any) => {
-      return prisma.category_Second.create({
+      return prisma.category_second.create({
         data: {
           name: args.name,
           parent: {
@@ -62,7 +62,7 @@ export default {
       });
     },
     CreateThirdCategory: (parent: any, args: any) => {
-      return prisma.category_Third.create({
+      return prisma.category_third.create({
         data: {
           name: args.name,
           parent: {
@@ -73,26 +73,26 @@ export default {
       });
     },
     UpdateFirstCategory: (parent: any, args: any) => {
-      return prisma.category_First.update({
+      return prisma.category_first.update({
         where: { id: args.id },
         data: { name: args.name },
       });
     },
     UpdateSecondCategory: (parent: any, args: any) => {
-      return prisma.category_Second.update({
+      return prisma.category_second.update({
         where: { id: args.id },
         data: { name: args.name },
       });
     },
     UpdateThirdCategory: (parent: any, args: any) => {
-      return prisma.category_Third.update({
+      return prisma.category_third.update({
         where: { id: args.id },
         data: { name: args.name },
       });
     },
     DeleteFirstCategory: async (parent: any, args: any) => {
       try {
-        await prisma.category_First.delete({ where: { id: args.id } });
+        await prisma.category_first.delete({ where: { id: args.id } });
         return true;
       } catch (error) {
         return false;
@@ -100,7 +100,7 @@ export default {
     },
     DeleteSecondCategory: async (parent: any, args: any) => {
       try {
-        await prisma.category_Second.delete({ where: { id: args.id } });
+        await prisma.category_second.delete({ where: { id: args.id } });
         return true;
       } catch (error) {
         return false;
@@ -108,7 +108,7 @@ export default {
     },
     DeleteThirdCategory: async (parent: any, args: any) => {
       try {
-        await prisma.category_Third.delete({ where: { id: args.id } });
+        await prisma.category_third.delete({ where: { id: args.id } });
         return true;
       } catch (error) {
         return false;
