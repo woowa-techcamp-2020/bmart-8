904df363b22885a41dc9b0ccf93a1100952fcf40
nohgijin <ynda11111@naver.com>
2020-08-16 23:33:13 +0900

    style: palette 색상 설정 공통으로 사용되는 색상을 설정

--- client/src/lib/styles/palette.ts
+++ client/src/lib/styles/palette.ts
@@ -1,2 +1,18 @@
 //이곳에 색상 설정
-export default {};
+/* colors */
+const palette = {
+  black: '#222222',
+  white: '#ffffff',
+  blue: '#0366d6',
+  danger: '#cc0000',
+
+  gray100: '#fafbfc',
+  gray200: '#eff1f3',
+  gray300: '#e1e4e8',
+  gray400: 'rgba(209, 213, 218, 0.5)',
+  gray500: '#586069',
+  gray700: '#6a737d',
+  gray900: '#24292e',
+};
+
+export default palette;
