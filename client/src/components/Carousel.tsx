fc757f1ee2951031c64debd21d8facbf4d3bb991
Atobaum <atobaum@gmail.com>
2020-08-18 11:00:23 +0900

    feat: Automatic carousel change

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const CarouselBlock = styled.div`
   width: 70%;
-  height: 200px;
+  height: 300px;
 
   .wrapper {
     display: flex;
-    overflow: auto;
+    overflow-x: auto;
+    overflow-y: hidden;
 
     width: 100%;
     height: 100%;
 
     scroll-snap-type: x mandatory;
     scroll-behavior: smooth;
+    & * {
+      box-sizing: border-box;
+    }
+
     div {
       flex-shrink: 0;
       scroll-snap-align: start;
@@ -21,18 +27,51 @@ const CarouselBlock = styled.div`
       padding: 0;
       width: 100%;
       height: 100%;
+      a {
+        display: block;
+        height: 100%;
+      }
+      img {
+        height: 100%;
+        width: 100%;
+      }
     }
   }
 `;
 
-const Carousel = () => {
+type CarouselProps = {
+  images: {
+    imageUrl: string;
+    altString: string;
+    routeUrl: string;
+  }[];
+};
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+  // to get width of the container
+  const containerRef = useRef(null);
+  const [curBanner, setCurBanner] = useState(0);
+  useEffect(() => {
+    const container = containerRef.current as any;
+    container.scroll(curBanner * container.offsetWidth, 0);
+
+    setTimeout(
+      () => setCurBanner(curBanner < images.length - 1 ? curBanner + 1 : 0),
+      3000
+    );
+  }, [curBanner]);
+
   return (
     <CarouselBlock>
-      <div className="wrapper">
-        <div style={{ background: 'red' }}>1</div>
-        <div style={{ background: 'gray' }}>2</div>
-        <div style={{ background: 'green' }}>3</div>
-        <div style={{ background: 'red' }}>4</div>
+      <div className="wrapper" ref={containerRef}>
+        {images.map(({ imageUrl, altString, routeUrl }, idx) => {
+          return (
+            <div key={idx}>
+              <Link to={routeUrl}>
+                <img src={imageUrl} alt={altString}></img>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </CarouselBlock>
   );
