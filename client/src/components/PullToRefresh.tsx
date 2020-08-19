64ac9aa9e3a8599ac58be7f456ce2856162fd5a5
Atobaum <atobaum@gmail.com>
2020-08-19 17:55:41 +0900

    feat: 당겼을 때 랜덤으로 메뉴 골라주기

--- client/src/components/PullToRefresh.tsx
+++ client/src/components/PullToRefresh.tsx
@@ -2,6 +2,21 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import addPointerEventHandlers from '../utils';
 
+const dummyMenu = ['밥', '돈까스', '카레', '초밥', '피자', '감자탕'];
+function getRandomMenu(menu: string[]) {
+  return menu[~~(Math.random() * menu.length)];
+}
+function getRandomMenus(
+  menu: string[],
+  number: number,
+  interval: number,
+  cb: any
+) {
+  if (number === 0) return;
+  cb(getRandomMenu(menu));
+  setTimeout(() => getRandomMenus(menu, number - 1, interval, cb), interval);
+}
+
 const PullToRefreshBlock = styled.div`
   width: 100%;
   border-top: 2px solid gray;
@@ -11,13 +26,17 @@ const PullToRefreshBlock = styled.div`
 type PullToRefreshProps = {
   onRefresh: () => void;
 };
+
 const PullToRefresh: React.FC<PullToRefreshProps> = ({ onRefresh }: any) => {
   const maxSize = 100;
   const [size, setSize] = useState(0);
   const [startY, setStartY] = useState<number | null>(null);
+  const [visible, setVisible] = useState(false);
+  const [menu, setMenu] = useState('');
   useEffect(() => {
     if (window.scrollY !== 0) return;
     const startHandler = (e: PointerEvent) => {
+      setVisible(true);
       setStartY(e.clientY);
     };
 
@@ -30,9 +49,17 @@ const PullToRefresh: React.FC<PullToRefreshProps> = ({ onRefresh }: any) => {
 
     const endHandler = (e: PointerEvent) => {
       if (size === maxSize) {
+        getRandomMenus(dummyMenu, 5, 300, setMenu);
+        setTimeout(() => {
+          setVisible(false);
+          setSize(0);
+          setMenu('');
+        }, 3500);
         onRefresh();
+      } else {
+        setVisible(false);
+        setSize(0);
       }
-      setSize(0);
       setStartY(null);
     };
     return addPointerEventHandlers(document, {
@@ -41,9 +68,9 @@ const PullToRefresh: React.FC<PullToRefreshProps> = ({ onRefresh }: any) => {
       onUp: endHandler,
     });
   }, [window.scrollY, startY, size]);
-  return size > 0 ? (
+  return visible ? (
     <PullToRefreshBlock style={{ height: `${size}px` }}>
-      {size === maxSize ? '땡겨요' : '놓아요'}
+      {menu} 땡겨요
     </PullToRefreshBlock>
   ) : null;
 };
