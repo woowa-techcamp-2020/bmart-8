d4b6a8f66980d020fbaa4a6f2dd2e243d623127e
Atobaum <atobaum@gmail.com>
2020-08-20 16:22:57 +0900

    refactor: Split search page components

---
+++ client/src/components/Search/SearchHistoryItem.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import { formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { Link } from 'react-router-dom';
+
+const SearchHistoryItemBlock = styled.li`
+  display: grid;
+  grid-template-columns: 1fr 6rem 1rem;
+  .link {
+    text-decoration: none;
+    color: black;
+    text-align: left;
+  }
+
+  .datetime {
+    color: gray;
+    text-align: left;
+    font-size: 1rem;
+  }
+
+  .delete-button {
+    background: none;
+    border: none;
+    font-size: 1rem;
+  }
+`;
+const SearchHistoryItem: React.FC<{
+  onDelete: () => void;
+  date: Date;
+  query: string;
+}> = ({ onDelete, date, query }) => {
+  return (
+    <SearchHistoryItemBlock>
+      <Link className="link" to={'/search/' + query}>
+        {query}
+      </Link>
+      <div className="datetime">
+        {formatDistanceToNow(date, { locale: ko })} 전
+      </div>
+      <button className="delete-button" onClick={onDelete}>
+        X
+      </button>
+    </SearchHistoryItemBlock>
+  );
+};
+export default SearchHistoryItem;
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,33 +1,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useQuery, useLazyQuery } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import { gql } from 'apollo-boost';
-import { formatDistanceToNow } from 'date-fns';
-import { ko } from 'date-fns/locale';
-import useDebounce from '../hooks/useDebounce';
-import { useHistory, Link } from 'react-router-dom';
-
-const SearchHistoryItem: React.FC<{
-  onDelete: () => void;
-  date: Date;
-  query: string;
-}> = ({ onDelete, date, query }) => {
-  return (
-    <div>
-      <div>
-        <Link to={'/search/' + query}>{query}</Link>
-      </div>
-      <div>{formatDistanceToNow(date, { locale: ko })} 전</div>
-      <button onClick={onDelete}>X</button>
-    </div>
-  );
-};
+import SearchInput from '../components/Search/SearchInput';
+import SearchRecommendationList from '../components/Search/SearchRecommendationList';
 
 const SearchPageBlock = styled.div``;
 
 const SearchPage: React.FC = () => {
-  const history = useHistory();
-  const [query, setQuery] = useState('');
+  const [instantSearchData, setInstantSearchData] = useState<string[]>([]);
   const { data: searchHistoryData } = useQuery(gql`
     query getSearchHistory {
       searchHistory {
@@ -37,52 +18,18 @@ const SearchPage: React.FC = () => {
       }
     }
   `);
-
-  const [instantSearch, { data: instantSearchData }] = useLazyQuery(gql`
-    query instantSearch($q: String) {
-      instantSearch(query: $q)
-    }
-  `);
-
-  // 분당 150타라고 가정. interval = 60*1000/150
-  const instantSearchDebounce = useDebounce(
-    (q: string) => instantSearch({ variables: { q } }),
-    400
-  );
-
   return (
     <SearchPageBlock>
-      <div>
-        <button onClick={() => history.goBack()}>go back</button>
-        <input
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
-            instantSearchDebounce(e.target.value);
-          }}
-        />
-      </div>
-      {instantSearchData
-        ? instantSearchData.instantSearch.map((name: string) => {
-            return (
-              <div key={name}>
-                <Link to={'/search/' + name}>{name}</Link>
-              </div>
-            );
-          })
-        : null}
-      {searchHistoryData
-        ? searchHistoryData.searchHistory.map(({ id, date, query }: any) => {
-            return (
-              <SearchHistoryItem
-                key={id}
-                onDelete={() => console.log('search histroy delete', id)}
-                date={new Date(date)}
-                query={query}
-              />
-            );
-          })
-        : null}
+      <SearchInput onSearch={setInstantSearchData} />
+      <SearchRecommendationList
+        instantSearchItems={instantSearchData}
+        searchHistoryItems={
+          searchHistoryData ? searchHistoryData.searchHistory : []
+        }
+        onDeleteSearshHistory={(id) =>
+          console.log('search history deleted. id: ', id)
+        }
+      />
     </SearchPageBlock>
   );
 };
