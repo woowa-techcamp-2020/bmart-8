97a371f858fd8cc440ee6780598e1b5bf06219e3
Atobaum <atobaum@gmail.com>
2020-08-20 13:30:35 +0900

    fix: Add id field to graphql type SearchHistory

--- server/src/graphql/resolvers/search.ts
+++ server/src/graphql/resolvers/search.ts
@@ -3,14 +3,17 @@ export default {
     searchHistory: () => {
       return [
         {
+          id: 1,
           query: '사과',
           date: new Date('2020-08-20'),
         },
         {
+          id: 2,
           query: '딸기',
           date: new Date('2020-08-19'),
         },
         {
+          id: 3,
           query: '모니터',
           date: new Date('2020-08-17'),
         },
