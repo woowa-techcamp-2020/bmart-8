2dcf29bccf9019d65c4195f3958da86ce47e0226
Atobaum <atobaum@gmail.com>
2020-08-25 11:30:51 +0900

    refactor: cart store 타입 명시

--- client/src/stores/cart-store.tsx
+++ client/src/stores/cart-store.tsx
@@ -1,5 +1,36 @@
 import React, { useReducer, useContext } from 'react';
 
+const INIT = 'INIT' as const;
+const SET_COUNT = 'SET_COUNT' as const;
+const DELETE_ONE = 'DELETE_ONE' as const;
+const DELETE_MANY = 'DELETE_MANY' as const;
+
+type InitAction = {
+  type: typeof INIT;
+  payload: CartItem[];
+};
+type SetCountAction = {
+  type: typeof SET_COUNT;
+  payload: {
+    id: number;
+    count: number;
+  };
+};
+type DeleteOneAction = {
+  type: typeof DELETE_ONE;
+  payload: { id: number };
+};
+type DeleteManyAction = {
+  type: typeof DELETE_MANY;
+  payload: { ids: number[] };
+};
+
+type CartStoreAction =
+  | InitAction
+  | SetCountAction
+  | DeleteOneAction
+  | DeleteManyAction;
+
 type CartItem = {
   id: number;
   product: {
@@ -16,21 +47,20 @@ type CartItem = {
 
 const initState: CartItem[] = [];
 
-const reducer = (state: CartItem[], action: any) => {
-  const payload = action.payload;
+const reducer = (state: CartItem[], action: CartStoreAction) => {
   switch (action.type) {
-    case 'INIT':
+    case INIT:
       return action.payload;
-    case 'SET_COUNT':
+    case SET_COUNT:
       return state.map((item) => {
-        return item.id === payload.id
-          ? { ...item, count: payload.count }
+        return item.id === action.payload.id
+          ? { ...item, count: action.payload.count }
           : item;
       });
-    case 'DELETE_ONE':
-      return state.filter((item) => item.id !== payload.id);
-    case 'DELETE_MANY':
-      return state.filter((item) => !payload.ids.includes(item.id));
+    case DELETE_ONE:
+      return state.filter((item) => item.id !== action.payload.id);
+    case DELETE_MANY:
+      return state.filter((item) => !action.payload.ids.includes(item.id));
     default:
       return state;
   }
@@ -54,6 +84,6 @@ export function useCartState(): CartItem[] {
   return useContext(StoreContext);
 }
 
-export function useCartDispatch() {
+export function useCartDispatch(): React.Dispatch<CartStoreAction> {
   return useContext(DispatchContext);
 }
