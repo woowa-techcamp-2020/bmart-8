3cad35eccb3a410c9408b79f730da48e315f17c6
Atobaum <atobaum@gmail.com>
2020-08-27 08:47:30 +0900

    refactor: server_build.yml의 내용을 build_server.sh로 이동
    - build_server.sh에서 tgz까지 생성

--- server/scripts/build_server.sh
+++ server/scripts/build_server.sh
@@ -1,3 +1,6 @@
 rm -rf build
 yarn build
 cp -r src/graphql/types build/graphql/types
+rm -rf node_modules
+yarn install --production
+tar -czf build.tgz node_modules appspec.yml build scripts Dockerfile package.json yarn.lock
