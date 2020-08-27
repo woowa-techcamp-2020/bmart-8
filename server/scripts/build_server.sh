87221b30f925b3d292094eb0c8b69a22c0e9ef21
Atobaum <atobaum@gmail.com>
2020-08-27 09:27:07 +0900

    fix: Add prisma file in build archive

--- server/scripts/build_server.sh
+++ server/scripts/build_server.sh
@@ -4,4 +4,4 @@ yarn build
 cp -r src/graphql/types build/graphql/types
 rm -rf node_modules
 yarn install --production
-tar -czf build.tgz node_modules appspec.yml build scripts Dockerfile package.json yarn.lock
+tar -czf build.tgz node_modules appspec.yml build prisma scripts Dockerfile package.json yarn.lock
